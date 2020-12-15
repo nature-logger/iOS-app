1cdce201c1b111e38f402d332d30bbb0768c19bc
Sebastian <43139409+semol18@users.noreply.github.com>
2020-12-15 16:50:37 +0100

    Storyboard separation (#11)
    * refactor storyboards into seperate boards
    
    * Update Search.storyboard
    
    Fixed storyboard ID error.
    
    Co-Authored-By: do-nat <47325790+do-nat@users.noreply.github.com>
    
    Co-authored-by: do-nat <47325790+do-nat@users.noreply.github.com>

---
+++ nature-logger/Controllers/SearchViewController.swift
@@ -0,0 +1,14 @@
+//
+//  APIController.swift
+//  nature-logger
+//
+//  Created by Donat on 15.12.20.
+//  Copyright © 2020 Syddansk Universitet. All rights reserved.
+//
+
+import UIKit
+
+
+class SearchViewController: UIViewController {
+    
+}
