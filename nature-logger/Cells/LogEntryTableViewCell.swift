daafb3fa506d3f551df018c9121db0e3e0d80d02
do-nat <47325790+do-nat@users.noreply.github.com>
2020-12-19 21:46:44 +0100

    use text view instead of label

--- nature-logger/Cells/LogEntryTableViewCell.swift
+++ nature-logger/Cells/LogEntryTableViewCell.swift
@@ -11,21 +11,9 @@ import UIKit
 class LogEntryTableViewCell: UITableViewCell {
 
     @IBOutlet weak var logEntryTitle: UILabel!
-    @IBOutlet weak var logEntryDescription: UILabel!
     @IBOutlet weak var logEntryImage: UIImageView!
+    @IBOutlet weak var logEntryDescription: UITextView!
     
     var poi: PointOfInterest?
     var tableImage: UIImage?
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
