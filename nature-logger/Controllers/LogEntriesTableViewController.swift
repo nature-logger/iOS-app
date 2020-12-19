c63e7ae7a764b753698ebed7920369ee79ce90cd
do-nat <47325790+do-nat@users.noreply.github.com>
2020-12-19 20:10:56 +0100

    test alternative way of loading

--- nature-logger/Controllers/LogEntriesTableViewController.swift
+++ nature-logger/Controllers/LogEntriesTableViewController.swift
@@ -10,29 +10,33 @@ import UIKit
 
 class LogEntriesTableViewController: UITableViewController {
     
+    var documentsUrl: URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    }
+    
     private var logEntries: LogEntries?
     var poi: PointOfInterest?
     let cellIdentifier = "logEntryTemplate"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         logEntries = LogEntries(poi)
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         guard logEntries != nil
@@ -41,7 +45,7 @@ class LogEntriesTableViewController: UITableViewController {
         }
         return logEntries!.pois.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? LogEntryTableViewCell, logEntries != nil else {
@@ -51,63 +55,19 @@ class LogEntriesTableViewController: UITableViewController {
         let poi = logEntries!.pois[indexPath.row]
         print(poi.imagePath)
         
-        if let imageValuePath = poi.filePath() {
-            if let image = UIImage.init(withName: imageValuePath) {
-                cell.logEntryImage.image = image
-            }
+        let fileURL = documentsUrl.appendingPathComponent("poiimage")
+        do {
+            let imageData = try Data(contentsOf: fileURL)
+            cell.logEntryImage.image =  UIImage(data: imageData)
+        } catch {
+            print("Error loading image : \(error)")
         }
+        
         
         cell.logEntryTitle.text = poi.title
         
         cell.logEntryDescription.text = poi.description
-
+        
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- nature-logger/Models/POIImage.swift
+++ nature-logger/Models/POIImage.swift
@@ -11,16 +11,30 @@ import UIKit
 
 class POIImage {
     private var image: UIImage
-    let id = UUID().uuidString
+    let id = "poiimage"
     var filename: String {
-        id + ".jpeg"
+        id
     }
+    
+    var documentsUrl: URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    }
+    
     
     init(image: UIImage) {
         self.image = image
     }
     
     func saveImage() {
-        self.image.saveImage(imageName: filename)
+        save(image: image)
+    }
+    
+    private func save(image: UIImage){
+        let fileName = id
+        let fileURL = documentsUrl.appendingPathComponent(fileName)
+        if let imageData = image.jpegData(compressionQuality: 1.0) {
+            try? imageData.write(to: fileURL, options: .atomic)
+        }
+        print("Error saving image")
     }
 }
